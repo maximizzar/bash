5424b2eaa9609abb5171e91f38c529ff0d995b37
maximizzar <60427093+maximizzar@users.noreply.github.com>
2024-03-13 18:18:26 +0100

    feat(wip): first factorio server init script

--- factorio/setup-server.sh
+++ factorio/setup-server.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
-read -p "Enter Server:" SERVER_NAME
-read -p "Enter Domainname:" DOMAIN_NAME
+read -p 'Enter Server:' SERVER_NAME
+read -p 'Enter Domainname:' DOMAIN_NAME
 
-read -p "Enter base dir (eg. srv): " BASE_DIR
-read -p "Enter a username, the server should run under." SERVER_USER
+read -p 'Enter base dir (eg. srv):' BASE_DIR
+read -p 'Enter a username, the server should run under:' SERVER_USER
 
 SYSTEMD_FACTORIO_SERVICES='etc/systemd/system/factorio'
 
 if ! [ id "$SERVER_USER" &>/dev/null ]; then
     useradd $SERVER_USER
-fi
-
-if ! [ command -v "sudo" >/dev/null 2>&1 ]; then
-    apt install sudo
 fi
 
 # setup systemd service
@@ -34,38 +30,41 @@ RestartSec=10
 WantedBy=multi-user.target
 EOF
 
-systemctl enable /$SYSTEMD_FACTORIO_SERVICES/$SERVER_NAME.$DOMAIN_NAME.service
+systemctl enable "/$SYSTEMD_FACTORIO_SERVICES/$SERVER_NAME.$DOMAIN_NAME.service"
 
 setup_server() {
     mkdir -pv /$BASE_DIR/$SERVER_NAME
-    cd /$BASE_DIR/$SERVER_NAME
-    wget -O "$SERVER_NAME.$DOMAIN_NAME" https://factorio.com/get-download/latest/headless/linux64
-    tar -xvf "$SERVER_NAME.$DOMAIN_NAME.tar" factorio/
-    chmod +x /$BASE_DIR/$SERVER_NAME.$DOMAIN_NAME/factorio/bin /x64/factorio
-    rm $SERVER_NAME.$DOMAIN_NAME.tar factorio/data/*example.json
+    cd "/$BASE_DIR/$SERVER_NAME"
+    wget -O "$(pwd)/$SERVER_NAME.$DOMAIN_NAME.tar" https://factorio.com/get-download/latest/headless/linux64
+    tar -xvf "$(pwd)/$SERVER_NAME.$DOMAIN_NAME.tar" "$(pwd)/factorio/"
+    chmod +x $(pwd)/$SERVER_NAME.$DOMAIN_NAME/factorio/bin/x64/factorio
+    rm $(pwd)/$SERVER_NAME.$DOMAIN_NAME.tar $(pwd)/factorio/data/*example.json
 }
 
-sudo -u $SERVER_USER setup_server
+setup_server
 
 # create update script
 touch "/$BASE_DIR/$SERVER_NAME/update.sh"
+
 cat <<EOF <"/$BASE_DIR/$SERVER_NAME/update.sh"
-systemctl stop $SERVER_NAME.$DOMAIN_NAME.service
+systemctl stop "$SERVER_NAME.$DOMAIN_NAME.service"
+su "$SERVER_USER"
 
 cd /$BASE_DIR/$SERVER_NAME
-sudo -u $SERVER_USER OLD_SERVER_VERSION=$'(./$BASE_DIR/$SERVER_NAME/factorio/bin/x64/factorio --version | grep -oP "Version: \K\d+\.\d+\.\d+")'
-sudo -u $SERVER_USER tar -cvf $OLD_SERVER_VERSION factorio
+OLD_SERVER_VERSION=$'(./$BASE_DIR/$SERVER_NAME/factorio/bin/x64/factorio --version | grep -oP "Version: \K\d+\.\d+\.\d+")'
+tar -cvf $OLD_SERVER_VERSION factorio
 
 setup_server() {
     mkdir -pv /$BASE_DIR/$SERVER_NAME
-    cd /$BASE_DIR/$SERVER_NAME
-    wget -O "$SERVER_NAME.$DOMAIN_NAME" https://factorio.com/get-download/latest/headless/linux64
-    tar -xvf "$SERVER_NAME.$DOMAIN_NAME.tar" factorio/
-    chmod +x /$BASE_DIR/$SERVER_NAME.$DOMAIN_NAME/factorio/bin /x64/factorio
-    rm $SERVER_NAME.$DOMAIN_NAME.tar factorio/data/*example.json
+    cd "/$BASE_DIR/$SERVER_NAME"
+    wget -O "$(pwd)/$SERVER_NAME.$DOMAIN_NAME.tar" https://factorio.com/get-download/latest/headless/linux64
+    tar -xvf "$(pwd)/$SERVER_NAME.$DOMAIN_NAME.tar" "$(pwd)/factorio/"
+    chmod +x $(pwd)/$SERVER_NAME.$DOMAIN_NAME/factorio/bin/x64/factorio
+    rm $(pwd)/$SERVER_NAME.$DOMAIN_NAME.tar $(pwd)/factorio/data/*example.json
 }
-
-sudo -u $SERVER_USER setup_server
-
+setup_server
+exit
 systemctl start $SERVER_NAME.$DOMAIN_NAME.service
 EOF
+
+chown "/$BASE_DIR/$SERVER_NAME.$DOMAIN_NAME -R $SERVER_USER:$SERVER_USER"
