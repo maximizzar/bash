a9b176c75d36c534cd8d614dcbe96cac38b25dcc
maximizzar <60427093+maximizzar@users.noreply.github.com>
2024-03-13 11:00:56 +0100

    fix: please stop breaking

--- debian_defaults.sh
+++ debian_defaults.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 echo "Only when .bashrc and .bash_aliases respectively removed, the script will pull configs remotely."
-read -m "Press Enter to proceed: "
+read -p "Press Enter to proceed: "
 
 echo "Set Bash config"
 wget --no-clobber https://raw.githubusercontent.com/maximizzar/bash/master/.bash_aliases
 wget --no-clobber https://raw.githubusercontent.com/maximizzar/bash/master/.bashrc
 
-if [ $UID != 0 ] then; {
+if [ $UID != 0 ]; then
     echo "Please login as admin"
     exit 1;
-}
+fi
 
 echo "Install repos"
+apt install -y gpg
+
 if ! [ -f "/etc/apt/sources.list.d/volian-archive-scar-unstable.list" ]; then
     echo "deb http://deb.volian.org/volian/ scar main" | tee /etc/apt/sources.list.d/volian-archive-scar-unstable.list
     wget -qO - https://deb.volian.org/volian/scar.key | tee /etc/apt/trusted.gpg.d/volian-archive-scar-unstable.gpg
