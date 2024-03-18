5beadbec00ee35240a21107f69540af341888ef6
maximizzar <60427093+maximizzar@users.noreply.github.com>
2024-03-18 07:45:32 +0100

    fix: undo git fail and switched to a loop to remove all example.json files

--- factorio/update.sh
+++ factorio/update.sh
@@ -1,43 +1,47 @@
 #!/bin/bash
+FACTORIO_REMOTE="https://factorio.com/get-download/latest/headless/linux64"
+FACTORIO_GAME_DIR="/home/factorio/k2se"
 
-echo "Set Vars"
-FACTORIO_REMOTE='https://factorio.com/get-download/latest/headless/linux64'
-FACTORIO_GAME_DIR='/home/factorio/k2se'
-
-echo "Halt Server"
-systemctl stop k2se.factorio.maximizzar.io.service
-
-if [ -e "${FACTORIO_GAME_DIR}" ]; then
-        echo "Archive Local Server"
-        cd "${FACTORIO_GAME_DIR}" || exit 1
-        SERVER_OLD_VERSION=$(/home/factorio/k2se/factorio/bin/x64/factorio --version | grep -oP 'Version: \K\d+\.\d+\.\d+')
-        SERVER_NEW_VERSION=$(wget -S --spider "${FACTORIO_REMOTE}" 2>&1 | grep 'Location' | awk '{print $2}' | grep -oP '(\d+\.\d+\.\d+)(?=_)')
-
-        echo "${SERVER_OLD_VERSION}" | hexdump -c
-        echo "${SERVER_NEW_VERSION}" | hexdump -c
-
-        if [ "${SERVER_OLD_VERSION}" = "${SERVER_NEW_VERSION}" ]; then
-                echo 'Server is upto Date!'
-                exit 1;
-        fi
-
-        # Backup old (local) Server-files
-        tar -cvf "factorio: ${SERVER_OLD_VERSION}.tar" factorio
-else
-        mkdir -pv "${FACTORIO_GAME_DIR}"
+# if first installation create dir
+if ! [ -e "${FACTORIO_GAME_DIR}" ]; then
+        echo "Start to init Server"
+        mkdir -pv ${FACTORIO_GAME_DIR}
 fi
 
-# Install new (remove) Server-files
-#wget -O "factorio.tar" "${FACTORIO_REMOTE}"
-#tar -xvf "factorio.tar" -C "factorio/"
-#chmod +x "${FACTORIO_GAME_DIR}/factorio/bin/x64/factorio"
+cd "${FACTORIO_GAME_DIR}" || exit 1
+SERVER_OLD_VERSION=$(/home/factorio/k2se/factorio/bin/x64/factorio --version | grep -oP 'Version: \K\d+\.\d+\.\d+')
+SERVER_NEW_VERSION=$(wget -S --spider "${FACTORIO_REMOTE}" 2>&1 | grep 'Location' | awk '{print $2}' | grep -oP '(\d+\.\d+\.\d+)' | head -1)
 
-# Cleanup installation
-#rm "factorio.tar"
+echo "local version: ${SERVER_OLD_VERSION}"
+echo "remote version: ${SERVER_NEW_VERSION}"
 
-if [[ -e "${FACTORIO_GAME_DIR}/factorio/data/*example.json" ]]; then
-        rm "/home/factorio/k2se/factorio/data/*example.json"
+if [ "${SERVER_OLD_VERSION}" == "${SERVER_NEW_VERSION}" ]; then
+        echo "Server is upto Date!"
+        exit 1;
 fi
 
-# Server restart
-systemctl start k2se.factorio.maximizzar.io.service
+echo "Start updating process"
+
+echo "Stop Server"
+sudo systemctl stop k2se.factorio.maximizzar.io.service
+
+echo "Backup old (local) Server-files"
+tar -cvf "factorio_${SERVER_OLD_VERSION}.tar" factorio
+
+echo "Install new (remove) Server-files"
+wget --no-verbose -O factorio.tar.xz ${FACTORIO_REMOTE}
+tar --overwrite -xvf factorio.tar.xz .
+chmod +x factorio/bin/x64/factorio
+
+echo "Cleanup installation"
+rm factorio.tar.xz
+
+for file in factorio/data/*example.json; do
+    if [[ -e "${file}" ]]; then
+        rm "${file}"
+    fi
+done
+
+echo "Start Server"
+sudo systemctl start k2se.factorio.maximizzar.io.service
+exit 0
