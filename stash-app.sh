21178c8f15498c59dea9595e8e20a7a7a727c237
maximizzar <60427093+maximizzar@users.noreply.github.com>
2024-03-19 04:57:11 +0100

    feat(wip): works w/o service creation

--- stash-app.sh
+++ stash-app.sh
@@ -1,26 +1,72 @@
 #!/bin/bash
 
-executable_name="stash-app"
-executable_path="/bin/$executable_name"
-executable_exists=$(command -v "$executable_name" >/dev/null 2>&1 && echo true || echo false)
+# get latest release tag from Github
+latest_release_api="https://api.github.com/repos/stashapp/stash/releases/latest"
+latest_release_tag=$(wget -qO- "${latest_release_api}" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 
-service_name="$executable_name.service"
-service_path="/etc/systemctl/system/$service_name"
-service_exists=$(command -v "cat $executable_path" >/dev/null 2>&1 && echo true || echo false)
+# you may want to change some vars to fit your environment better
+EXECUTABLE="stash-app"
+EXECUTABLE_EXISTS=$(command -v "${EXECUTABLE}" >/dev/null 2>&1 && echo true || echo false)
+if [ "${EXECUTABLE_EXISTS}" ]; then
+        EXECUTABLE_VERSION=$(${EXECUTABLE} --version | grep -oP 'v\d+\.\d+\.\d+')
+        if [[ "${EXECUTABLE_VERSION}//v/" == *"${latest_release_tag}"* ]]; then
+                echo "Latest Version is already installed."
+                exit 1
+        fi
+fi
+SERVICE="/etc/systemd/system/${EXECUTABLE}.service"
 
-if [ "$executable_exists" ]; then
-	executable_version=$($executable_name --version)
+echo "latest_release_api: ${latest_release_api}"
+echo "latest_release_tag: ${latest_release_tag}"
+
+echo "EXECUTABLE: ${EXECUTABLE}"
+echo "EXECUTABLE_EXISTS: ${EXECUTABLE_EXISTS}"
+echo "EXECUTABLE_VERSION: ${EXECUTABLE_VERSION}"
+echo "SERVICE: ${SERVICE}"
+
+read -n 1 -s -r -p "If you're okay with those settings, press any key to continue..."
+
+if  [ -e "${SERVICE}" ]; then
+    systemctl stop ${EXECUTABLE}.service
+fi
+
+if [ "${EXECUTABLE_EXISTS}" ]; then
+        # Move old version
+        mv "/bin/${EXECUTABLE}" "./${EXECUTABLE}-${EXECUTABLE_VERSION}"
+fi
+
+# Set correct architecture based on the System
+case "$(uname -m)" in
+x86_64)
+        arch="stash-linux"
+        ;;
+armel)
+        arch="stash-linux-arm32v6"
+        ;;
+armhf)
+        arch="stash-linux-arm32v7"
+        ;;
+arm64)
+        arch="stash-linux-arm64v8"
+        ;;
+esac
+
+# Get new Version from github and make it executable
+wget --no-verbose --continue --show-progress --no-dns-cache --xattr --content-disposition -O "/bin/${EXECUTABLE}" "https://github.com/stashapp/stash/releases/download/${latest_release_tag}/${arch}"
+chmod +x "/bin/${EXECUTABLE}"
+
+if  [ -e "${SERVICE}" ]; then
+    systemctl start ${EXECUTABLE}.service
+    exit 0
 fi
 
 # build service
-if ! [ "$service_exists" ]; then
-
-cat > "$service_path" <<EOF
+cat >"/etc/systemd/system/${EXECUTABLE}.service" <<EOF
 [Unit]
-Description=$executable_name Server
+Description=Stash-app service
 
 [Service]
-ExecStart=$service_path
+ExecStart=/bin/${EXECUTABLE}
 User=root
 Type=simple
 Restart=on-failure
@@ -28,45 +74,7 @@ RestartSec=10
 
 [Install]
 WantedBy=multi-user.target
-
 EOF
 
-	systemctl enable $service_name
-else
- 	systemctl stop $service_name
-fi
-
-# get latest release tag from Github
-latest_release_api="https://api.github.com/repos/stashapp/stash/releases/latest"
-latest_release_tag=$(curl --silent "$latest_release_api" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
-
-if [ "$executable_exists" ]; then
-	if [[ "$executable_version//v/" == *"${latest_release_tag}"* ]]; then
-		echo "Latest Version is already installed."
-		exit 1
-	fi
-	# Move old version in service_users home dir
-	mv "/bin/$executable_name" "$HOME/$executable_name:$executable_version"
-fi
-
-# Set correct architecture based on the System
-case "$(uname -m)" in
-x86_64)
-	arch="stash-linux"
-	;;
-armel)
-	arch="stash-linux-arm32v6"
-	;;
-armhf)
-	arch="stash-linux-arm32v7"
-	;;
-arm64)
-	arch="stash-linux-arm64v8"
-	;;
-esac
-
-# Get new Version from github and make it executable
-wget -O "$executable_path" "https://github.com/stashapp/stash/releases/tag/$latest_release_tag/$arch"
-chmod +x "$executable_path"
-
-exit 0
+systemctl enable "${EXECUTABLE}.service"
+systemctl start "${EXECUTABLE}.service"
