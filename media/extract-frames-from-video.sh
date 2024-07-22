8d6bc9faedb2c7bf985c0a60928f0215c705e3d3
maximizzar <mail@maximizzar.de>
2024-07-22 11:38:59 +0200

    feat: add new media function

---
+++ media/extract-frames-from-video.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+if ! [ -f "$1" ]; then
+        echo "file '$1' does not exist"
+        exit 1
+fi
+
+# Provide a dir name as 2nd Argument.
+# No trailing Slash please!
+if [ "$2" == "" ]; then
+        frames="frames"
+else
+        frames="$2"
+fi
+
+# Total number of frames
+total_frames=$(ffprobe -v error -count_frames -select_streams v:0 -show_entries stream=nb_read_frames -of default=nokey=1:noprint_wrappers=1 -i "$1")
+
+# Calculate the number of digits required
+num_digits=$(echo -n "$total_frames" | wc -c)
+
+# Create format string for FFmpeg
+format_string=$(printf "%%0%dd.png" "$num_digits")
+
+mkdir "$frames"
+ffmpeg -i "$1" "$frames/${format_string}"
