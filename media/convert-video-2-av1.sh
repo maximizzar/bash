e02c83f9d34f771a375a4a61d976777374116597
maximizzar <mail@maximizzar.de>
2024-08-13 21:12:45 +0200

    feat: add check_codec to only re-encode non av1 videos

--- media/convert-video-2-av1.sh
+++ media/convert-video-2-av1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# reencodes only the video stream to av1.
+# re-encodes only the video stream to av1.
 
 # Make sure every Program is installed for the script to run!
 if ! [ "$(command -v ffmpeg)" ]; then
@@ -7,21 +7,40 @@ if ! [ "$(command -v ffmpeg)" ]; then
         exit 1
 fi
 
+check_codec() {
+        local file=$1
+        if echo ffprobe -v error -select_streams v:0 -show_entries stream=codec_name -of default=noprint_wrappers=1:nokey=1 "$file" | grep -q "av1"; then
+                return 0
+        fi
+        return 1
+}
+
 mkdir re-encode
 
 # Only a single file
 if [ "$1" ]; then
+
     file="$1"
-    ffmpeg -i "$file" -map 0 -c copy -c:v libsvtav1 -crf 23 -b:v 0 "re-encode/$file"
-    exit 0
+    if check_codec "$file" -eq 0; then
+            ffmpeg -i "$file" -map 0 -c copy -c:v libsvtav1 -crf 23 -b:v 0 "re-encode/$file"
+            exit 0
+    fi
+    exit 1
 fi
 
 # Iterate through the files in the directory and re-encode
+
 for file in *; do
-    echo "$file"
-    if [ -f "$file" ]; then
-      if ! [ -f "re-encode/$file" ]; then
-        ffmpeg -i "$file" -map 0 -c copy -c:v libsvtav1 -crf 23 -b:v 0 "re-encode/$file"
-      fi
-    fi
+        echo "$file"
+        if [ -f "$file" ]; then
+                if [ -f "re-encode/$file" ]; then
+                        continue
+                fi
+
+                if check_codec "$file" -eq 1; then
+                        continue
+                fi
+
+                ffmpeg -i "$file" -map 0 -c copy -c:v libsvtav1 -crf 23 -b:v 0 "re-encode/$file"
+        fi
 done
